5582d80f048f4ae0cf07a7ba91ef190e2ca23f59
ChaoticSteve <tuninpavel@gmail.com>
2021-10-06 11:00:44 +0600

    wrote code in task_1_1.py

---
+++ Tyunin_Pavel_dz_1/task_1_1.py
@@ -0,0 +1,22 @@
+try:
+    duration = int(input('Введите продолжительность в секундах: '))
+    if duration < 60:
+        sec = duration
+        print(f'{sec} сек')
+    elif 60 <= duration < 3600:  # равносильно записи duration >= 60 and duration < 3600
+        minute = duration // 60
+        sec = duration % 60
+        print(f'{min} мин {sec} сек')
+    elif 3600 <= duration < 86400:  # равносильно записи duration >= 3600 and duration < 86400
+        hour = duration // 3600
+        minute = (duration % 3600) // 60
+        sec = (duration % 3600) % 60
+        print(f'{hour} час {minute} мин {sec} сек')
+    else: # во всех остальных случаях
+        day = duration // 86400
+        hour = (duration % 86400) // 3600
+        minute = ((duration % 3600) // 60)
+        sec = (duration % 3600) % 60
+        print(f'{day} дн {hour} час {minute} мин {sec} сек')
+except ValueError:
+    print('Нужно ввести числовое значение')
