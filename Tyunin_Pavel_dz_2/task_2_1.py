3c1d5ebf7a2a54534f3caa4dd869adb6a87cde63
ChaoticSteve <tuninpavel@gmail.com>
2021-10-09 18:03:27 +0600

    wrote code in task_2_1.py

---
+++ Tyunin_Pavel_dz_2/task_2_1.py
@@ -0,0 +1,8 @@
+print(type(15 * 3))
+print(isinstance(15 * 3, int))
+print(type(15 / 3))
+print(isinstance(15 / 3, float))
+print(type(15 // 2))
+print(isinstance(15 // 2, int))
+print(type(15 ** 2))
+print(isinstance(15 ** 2, int))
