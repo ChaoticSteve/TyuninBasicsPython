866492b029e2633fe4fea56bfcf0d08fb33383ce
ChaoticSteve <tuninpavel@gmail.com>
2021-10-11 18:35:21 +0600

    modified task_2_4.py

--- Tyunin_Pavel_dz_2/task_2_4.py
+++ Tyunin_Pavel_dz_2/task_2_4.py
@@ -1,6 +1,4 @@
 staff = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА', 'токарь высшего разраяда нИКОЛАЙ', 'директор аэлита']
 for i in range(len(staff)):
-    staff[i] = staff[i].title()
-    staff[i] = staff[i].split(' ')
+    staff[i] = staff[i].title().split(' ')
     print(f'Привет, {staff[i][len(staff[i]) - 1]}!')
-print(staff)
