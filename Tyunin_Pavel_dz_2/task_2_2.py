ca5d147e470d683d7142dd81a31577d1b1c30544
ChaoticSteve <tuninpavel@gmail.com>
2021-10-11 18:33:49 +0600

    modified code in task_2_2.py

--- Tyunin_Pavel_dz_2/task_2_2.py
+++ Tyunin_Pavel_dz_2/task_2_2.py
@@ -6,12 +6,8 @@ while i < len(raw_massage):
         sign = raw_massage[i][0]
     if raw_massage[i].isdigit() or (sign and raw_massage[i][1:].isdigit()):
         if sign:
-            raw_massage[i] = sign + raw_massage[i][1:].zfill(2)
+            raw_massage[i] = '"' + sign + raw_massage[i][1:].zfill(2) + '"'
         else:
-            raw_massage[i] = raw_massage[i].zfill(2)
-        raw_massage.insert(i, '"')
-        raw_massage.insert(i + 2, '"')
-        i += 2
+            raw_massage[i] = '"' + raw_massage[i].zfill(2) + '"'
     i += 1
-print(raw_massage)
 print(' '.join(raw_massage))
