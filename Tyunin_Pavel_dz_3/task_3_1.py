27de0982daa5750a119e3d8d8e30e9043d3e1c4c
ChaoticSteve <tuninpavel@gmail.com>
2021-10-15 10:20:05 +0600

    wrote code tak_3_1.py

---
+++ Tyunin_Pavel_dz_3/task_3_1.py
@@ -0,0 +1,15 @@
+def num_translate_adv(name):
+    if name.istitle():
+        digit_translate = {'One': 'Один', 'Two': 'Два',
+                           'Three': 'Три', 'Four': 'Четыре',
+                           'Five': 'Пять', 'Six': 'Шесть',
+                           'Seven': 'Семь', 'Eight': 'Восемь',
+                           'Nine': 'Девять', 'Ten': 'Десять'}
+        print(digit_translate.get(name))
+    else:
+        digit_translate = {'one': 'один', 'two': 'два',
+                           'three': 'три', 'four': 'четыре',
+                           'five': 'пять', 'six': 'шесть',
+                           'seven': 'семь', 'eight': 'восемь',
+                           'nine': 'девять', 'ten': 'десять'}
+        print(digit_translate.get(name))
