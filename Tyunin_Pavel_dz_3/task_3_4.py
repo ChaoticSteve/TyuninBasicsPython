b5dc44d42172d628563f6e3a86dea5c05d8726fc
ChaoticSteve <tuninpavel@gmail.com>
2021-10-15 14:01:10 +0600

    modified task_3_4.py

--- Tyunin_Pavel_dz_3/task_3_4.py
+++ Tyunin_Pavel_dz_3/task_3_4.py
@@ -1,7 +1,16 @@
+def thesaurus(names):
+    my_dict_2 = {}
+    for name in names:
+        if name[0][0] not in my_dict_2:
+            my_dict_2[name[0][0]] = [' '.join(name)]
+        else:
+            my_dict_2[name[0][0]].append(' '.join(name))
+    return my_dict_2
+
+
 def thesaurus_adv(*names):
     my_list = []
     my_dict = {}
-    my_dict_2 = {}
     for name in names:
         my_list.append(name.split(' '))
     for name in my_list:
@@ -10,13 +19,7 @@ def thesaurus_adv(*names):
         else:
             my_dict[name[1][0]].append(name)
     for key in my_dict:
-        for name in my_dict[key]:
-            if name[0][0] not in my_dict_2:
-                my_dict_2[name[0][0]] = [' '.join(name)]
-            else:
-                my_dict_2[name[0][0]].append(' '.join(name))
-        my_dict[key] = my_dict_2.copy()
-        my_dict_2.clear()
+        my_dict[key] = thesaurus(my_dict[key])
     return my_dict
 
 
