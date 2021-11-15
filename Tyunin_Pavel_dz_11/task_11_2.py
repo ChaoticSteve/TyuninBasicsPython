4fa17091b2bd8a957dfa3a3822ba679d408fb37e
ChaoticSteve <tuninpavel@gmail.com>
2021-11-15 12:13:54 +0600

    update task_11_2.py

--- Tyunin_Pavel_dz_11/task_11_2.py
+++ Tyunin_Pavel_dz_11/task_11_2.py
@@ -1,4 +1,6 @@
 class ZeroDivision(Exception):
+    # def __init__(self, text):
+    #     self.text = text
     def __str__(self):
         return f'На 0 делить нелья'
 
@@ -13,3 +15,17 @@ else:
     num_2 = int(input('Введите делимое: '))
     print(f'{(num_2 / num):.2f}')
 
+'''Еще пришел в голову такой вариант, но удадёт с ошибкой, если забыть передать текст ошибки в блок except'''
+# class ZeroDivision(Exception):
+#     def __init__(self, text):
+#         self.text = text
+#
+# try:
+#     num = int(input('Введите делитель: '))
+#     if num == 0:
+#         raise ZeroDivision('Делить на 0 нельзя')
+# except ZeroDivision as e:
+#     print(e)
+# else:
+#     num_2 = int(input('Введите делимое: '))
+#     print(f'{(num_2 / num):.2f}')
