43406c00fa4b48a789b3637606db997375783a73
Pavel Tyunin <91519022+ChaoticSteve@users.noreply.github.com>
2021-11-23 20:25:18 +0600

    Update task_9_2.py

--- Tynin_Pavel_dz_9/task_9_2.py
+++ Tynin_Pavel_dz_9/task_9_2.py
@@ -3,7 +3,7 @@ class Road:
         self._length = length
         self._width = width
 
-    def mass(self, mass_in_m2, depth):
+    def get_mass(self, mass_in_m2, depth):
         return f'{(self._length * self._width * mass_in_m2 * depth) / 1000:.2f} т'
 
 
